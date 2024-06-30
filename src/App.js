da2a4d8ec8535effa668e2b223dab19b52a86f72
Chiril Dones <doneskirill3@gmail.com>
2024-06-30 15:16:01 +0300

    init

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   },[])
   return (
     <div className="App">
-      <button onClick={onToggleButton}></button>
+      <button onClick={onToggleButton}>toggle</button>
     </div>
   );
 }
